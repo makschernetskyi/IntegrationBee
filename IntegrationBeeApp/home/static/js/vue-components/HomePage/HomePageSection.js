8bd6a353b0bb3a59544379a71f07d2d32033ecb8
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2024-02-12 00:35:05 +0100

    frontend homepage updated

--- IntegrationBeeApp/home/static/js/vue-components/HomePage/HomePageSection.js
+++ IntegrationBeeApp/home/static/js/vue-components/HomePage/HomePageSection.js
@@ -2,7 +2,7 @@
 
 export default {
     template: `
-        <section>
+        <section class="Home-Section">
             <slot></slot>
         </section> 
     `
--- IntegrationBeeApp/home/static/js/vue-components/HomePage/Homepage.js
+++ IntegrationBeeApp/home/static/js/vue-components/HomePage/Homepage.js
@@ -7,13 +7,58 @@ export default {
     },
     template: `
         <div class="Home">
-            <HomePageSection>
-                <h1>Integration Bee in Vienna!</h1>
-                <p>Famous integration bee</p>
+            <HomePageSection class="Home-IntroSection">
+                <div class="Home-IntroSection-Illustration">
+                    <img src="static/assets/homepic_crop.jpg" alt="">
+                </div>
+                <div class="Home-IntroSection-Description">
+                    <div class="Home-IntroSection-Description-Title">
+                        <h1>Integration Bee now in Vienna!</h1>
+<!--                        <p>Integration bee format event is coming to Vienna!!!</p>-->
+                    </div>
+                    <div class="Home-IntroSection-Description-About">
+                        <p>Test your calculus skills and discover the world
+                        of competetive mathematics!
+                        </p>
+                    </div>
+                    
+                    <div class="Home-IntroSection-Description-Invitation">
+                        <p>Want to join?</p> <router-link to="/signUp">Sign Up</router-link>
+                    </div>
+                    
+                </div>
             </HomePageSection>
-            <HomePageSection>
-                <h1>haha</h1>
-                <p>huhu</p>
+            <HomePageSection class="Home-MotivationSection">
+                <h2 class="Home-MotivationSection-header">
+                    Why to Participate?
+                </h2>
+                <ul class="Home-MotivationSection-ReasonsList">
+                    <li class="Home-MotivationSection-ReasonsList-Item">
+                        <h3 class="Home-MotivationSection-ReasonsList-Item-header">
+                        Compete!
+                        </h3>
+                        <p class="Home-MotivationSection-ReasonsList-Item-info">
+                        Integration bee is a format where you can compete with other
+                        people in solving integrals!
+                        </p>
+                    </li>
+                    <li class="Home-MotivationSection-ReasonsList-Item">
+                        <h3 class="Home-MotivationSection-ReasonsList-Item-header">
+                        Make friends!
+                        </h3>
+                        <p class="Home-MotivationSection-ReasonsList-Item-info">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur distinctio eius eos exercitationem itaque labore mollitia odio odit quas quis!
+                        </p>
+                    </li>
+                    <li class="Home-MotivationSection-ReasonsList-Item">
+                        <h3 class="Home-MotivationSection-ReasonsList-Item-header">
+                        Win Prizes!
+                        </h3>
+                        <p class="Home-MotivationSection-ReasonsList-Item-info">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum incidunt perferendis sit ullam. Doloribus, et!
+                        </p>
+                    </li>
+                </ul>
             </HomePageSection>
         </div>
 `
