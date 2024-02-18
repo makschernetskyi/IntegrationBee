4984f5333dbf693f59dfd197511d332062cf38e9
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2024-02-18 16:21:31 +0100

    wagtail homepage and newspage added

---
+++ IntegrationBeeApp/home/blocks.py
@@ -0,0 +1,23 @@
+
+
+from wagtail import blocks
+
+
+class HomeBulletPointBlock(blocks.StructBlock):
+    """header and text"""
+
+    header = blocks.CharBlock(required=True, help_text="add header")
+    text = blocks.TextBlock(required=True, help_text="add text")
+
+    class Meta:
+        label = "Bullet Point"
+
+class NewsBlock(blocks.StructBlock):
+    """header and text"""
+
+    header = blocks.CharBlock(required=True, help_text="add header")
+    text = blocks.TextBlock(required=True, help_text="add text")
+    picture = blocks.TextBlock(required=False, help_text="add image")
+
+    class Meta:
+        label = "News"
--- IntegrationBeeApp/home/models.py
+++ IntegrationBeeApp/home/models.py
@@ -1,7 +1,51 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+
+from . import blocks
 
 
 class HomePage(Page):
+    """Home Page Model"""
+
     template = "home/home.html"
+    max_count = 1
+
+    title_section_header = models.CharField(max_length=50, blank=False, null=True)
+    title_section_description = RichTextField(features=["bold", "link"], null=True)
+
+    bullet_points_section_header = models.CharField(max_length=50, blank=False, null=True)
+
+    bullet_points = StreamField(
+        [
+            ("home_bullet_point", blocks.HomeBulletPointBlock())
+        ],
+        use_json_field=True,
+        null=True,
+        blank=True
+    )
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("title_section_header"),
+        FieldPanel("title_section_description"),
+        FieldPanel("bullet_points_section_header"),
+        FieldPanel("bullet_points")
+    ]
+
+
+class NewsPage(Page):
+    """News Page Model"""
+
+    template = "home/home.html"
+
+    page_title = models.CharField(max_length=50, blank=False, null=True)
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel("page_title")
+    ]
+
