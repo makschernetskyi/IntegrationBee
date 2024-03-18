5fdccbb43a629da442e12a57b08931970fa15a89
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2024-03-18 23:25:12 +0100

    settings modified

--- IntegrationBeeApp/IntegrationBeeApp/settings/base.py
+++ IntegrationBeeApp/IntegrationBeeApp/settings/base.py
@@ -169,7 +169,7 @@ SASS_OUTPUT_STYLE = 'compact'
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/5.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
