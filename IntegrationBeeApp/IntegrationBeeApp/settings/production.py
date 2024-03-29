7ac1e1622fe15f4bf1992edc86e1cf77c6cb59ad
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2024-03-29 22:15:33 +0100

    settings updated

--- IntegrationBeeApp/IntegrationBeeApp/settings/production.py
+++ IntegrationBeeApp/IntegrationBeeApp/settings/production.py
@@ -2,6 +2,16 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "IntegrationBeeDB",
+        "USER": "massraum",
+        "PASSWORD": "12345678",
+        "HOST": "massraum.mysql.eu.pythonanywhere-services.com",
+    }
+}
+
 try:
     from .local import *
 except ImportError:
