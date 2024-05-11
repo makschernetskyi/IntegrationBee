eb9e7a9c8ef872baabde2f48afeaa8c6696eee66
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2024-05-11 18:03:20 +0200

    feat(frontend): introduce admin page frontend

---
+++ Frontend/AdminPanelFrontend/eslint.config.mjs
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	...pluginVue.configs['flat/recommended'],
+	eslintConfigPrettier,
+	{
+		plugins: {
+			'typescript-eslint': tseslint.plugin,
+		},
+		languageOptions: {
+			parserOptions: {
+				parser: tseslint.parser,
+				extraFileExtensions: ['.vue'],
+				sourceType: 'module',
+			},
+		},
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'no-dupe-keys': 'off',
+		},
+		ignores: ['./node_modules/*', 'eslint.config.mjs', 'src/presets/*'],
+	}
+);
---
+++ Frontend/AdminPanelFrontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Message from 'primevue/message';
+</script>
+
+<template>
+	Hello Admin Page
+	<Message>Hello World</Message>
+</template>
