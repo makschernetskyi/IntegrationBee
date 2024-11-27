6864d9adc1a62cef0ffc8b3bfd2ab858b2769633
Franciszek Lajszczak <f.lajszczak@gmail.com>
2024-11-27 18:10:58 +0100

    latex hotfix

--- IntegrationBeeApp/api/templatetags/custom_filters.py
+++ IntegrationBeeApp/api/templatetags/custom_filters.py
@@ -2,6 +2,9 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def print_difficulty(difficulty_level):
-    return "● " * difficulty_level + "◌ " * (10 - difficulty_level)
+    filled_circles = "\\ding{108} " * difficulty_level
+    empty_circles = "\\ding{109} " * (10 - difficulty_level)
+    return filled_circles + empty_circles
